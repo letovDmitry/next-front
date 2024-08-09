ef1bb5df204b99321f4e99be89b42007181978c7
letovDmitry <67585149+letovDmitry@users.noreply.github.com>
2024-08-09 23:44:18 +0600

    notification

--- src/app/payment-success/page.tsx
+++ src/app/payment-success/page.tsx
@@ -11,7 +11,24 @@ export const metadata: Metadata = {
   };
 
 
-const PaymentSuccess = () => {
+const PaymentSuccess = ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) => {
+  if (Object.keys(searchParams).includes('error')) return (
+    <div className={styles.notFound}>
+        <div className='container'>
+            <div className={styles.body}>
+               <Image src="./error.svg" alt="" width={100} height={100} quality={100}/>
+                <h1>Что-то пошло не так,</h1>
+                <span>Проверьте введенные данные и повторите попытку еще раз</span> 
+                <Link href='/'>Назад</Link>
+            </div>
+        </div>
+    </div>
+  )
+
   return (
     <div className={styles.notFound}>
         <div className='container'>
